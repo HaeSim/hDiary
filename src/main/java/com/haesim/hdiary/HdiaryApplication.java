55433bff440923e0a46df181548450e7db06f61b
HaeSim <anjin309@gmail.com>
2020-04-19 22:43:50 +0900

    최초커밋

---
+++ src/main/java/com/haesim/hdiary/HdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.haesim.hdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haesim/hdiary/HdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haesim.hdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
